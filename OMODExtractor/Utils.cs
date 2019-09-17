330c3b32d8e6fe208554bc65f13fb9f155bf0ec1
erri120 <erri120@users.noreply.github.com>
2019-09-17 15:01:43 +0200

    Added DeleteDir function

--- OMODExtractor/Utils.cs
+++ OMODExtractor/Utils.cs
@@ -25,21 +25,27 @@ namespace OMODExtractor
         {
             foreach (var item in tempDirs)
             {
-                if (Directory.Exists(item))
-                {
-                    DirectoryInfo di = new DirectoryInfo(item);
-                    foreach (FileInfo file in di.GetFiles())
-                    {
-                        file.Delete();
-                    }
-                    foreach (DirectoryInfo dir in di.GetDirectories())
-                    {
-                        dir.Delete(true);
-                    }
-                    Directory.Delete(item);
-                }
+                DeleteDir(item);
             }
         }
+
+        public void DeleteDir(string item)
+        {
+            if (Directory.Exists(item))
+            {
+                DirectoryInfo di = new DirectoryInfo(item);
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+                Directory.Delete(item);
+            }
+        }
+
         public void SaveToFile(string contents, string dest)
         {
             if (File.Exists(dest)) File.Delete(dest);
