9b8215a000098c24f20fb060476facb953fb841b
erri120 <erri120@users.noreply.github.com>
2019-09-20 12:59:40 +0200

    Created OMODInstaller project

---
+++ OMODInstaller/Installer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMODInstaller
+{
+    class Installer
+    {
+        static void Main(String[] args)
+        {
+
+        }
+    }
+}
