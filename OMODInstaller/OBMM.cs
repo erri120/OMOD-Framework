47fad40b6dcdb405e502fb9e8030b2bf611316f3
erri120 <erri120@protonmail.com>
2019-09-27 21:01:15 +0200

    Implemented file and folder safe checks

--- OMODInstaller/OBMM.cs
+++ OMODInstaller/OBMM.cs
@@ -20,5 +20,28 @@ namespace OblivionModManager
         internal static readonly string CurrentDir = (Path.GetDirectoryName(Application.ExecutablePath) + "\\").ToLower();
         internal static readonly string OblivionINIDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "My games\\oblivion\\");
         internal static readonly string OblivionESPDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "oblivion\\");
+
+        internal static bool IsSafeFileName(string s)
+        {
+            s = s.Replace('/', '\\');
+            if (s.IndexOfAny(Path.GetInvalidPathChars()) != -1) return false;
+            if (Path.IsPathRooted(s)) return false;
+            if (s.StartsWith(".") || Array.IndexOf<char>(Path.GetInvalidFileNameChars(), s[0]) != -1) return false;
+            if (s.Contains("\\..\\")) return false;
+            if (s.EndsWith(".") || Array.IndexOf<char>(Path.GetInvalidFileNameChars(), s[s.Length - 1]) != -1) return false;
+            return true;
+        }
+
+        internal static bool IsSafeFolderName(string s)
+        {
+            if (s.Length == 0) return true;
+            s = s.Replace('/', '\\');
+            if (s.IndexOfAny(Path.GetInvalidPathChars()) != -1) return false;
+            if (Path.IsPathRooted(s)) return false;
+            if (s.StartsWith(".") || Array.IndexOf<char>(Path.GetInvalidFileNameChars(), s[0]) != -1) return false;
+            if (s.Contains("\\..\\")) return false;
+            if (s.EndsWith(".")) return false;
+            return true;
+        }
     }
 }
