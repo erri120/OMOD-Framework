c86d7f01435c7190e22b25dded91e8b18fbdb2fb
erri120 <erri120@protonmail.com>
2019-09-27 20:34:54 +0200

    Start of Application wide implementation

---
+++ OMODInstaller/OBMM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace OblivionModManager
+{
+    internal static class Program
+    {
+        internal const string version = "1.1.12"; // latest official obmm release
+        internal const byte MajorVersion = 1;
+        internal const byte MinorVersion = 1;
+        internal const byte BuildNumber = 12;
+        internal const byte CurrentOmodVersion = 4;
+
+        internal static string TempDir
+        {
+            get { return Path.GetTempPath() + @"obmm\"; }
+        }
+
+        internal static readonly string CurrentDir = (Path.GetDirectoryName(Application.ExecutablePath) + "\\").ToLower();
+        internal static readonly string OblivionINIDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "My games\\oblivion\\");
+        internal static readonly string OblivionESPDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "oblivion\\");
+    }
+}
