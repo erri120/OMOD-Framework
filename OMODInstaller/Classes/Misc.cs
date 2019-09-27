125d716e07c4058d978420708f9b708fc570b9e1
erri120 <erri120@protonmail.com>
2019-09-27 19:41:44 +0200

    Imported Public and Misc classes from OBMM

---
+++ OMODInstaller/Classes/Misc.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace OblivionModManager
+{
+    public enum ConflictLevel { Active, NoConflict, MinorConflict, MajorConflict, Unusable }
+    public enum DeactiveStatus { Allow, WarnAgainst, Disallow }
+}
