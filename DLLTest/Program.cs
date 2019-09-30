827de25f8a54645d2d096c1c0c78523d77a38fd9
erri120 <erri120@protonmail.com>
2019-09-30 19:42:49 +0200

    Made the example better

--- DLLTest/Program.cs
+++ DLLTest/Program.cs
@@ -10,19 +10,33 @@ namespace DLLTest
         static void Main(string[] args)
         {
             Framework f = new Framework();
-            f.SetDataDirectory("");
-            f.SetOblivionDirectory("");
-            f.SetOblivionINIPath("");
+            string data = @"S:\SteamLibrary\steamapps\common\Oblivion\data";
+            f.SetDataDirectory(data);
+            f.SetOblivionDirectory(@"S:\SteamLibrary\steamapps\common\Oblivion");
+            f.SetOblivionINIPath(@"C:\Users\Florian\Documents\My Games\Oblivion\Oblivion.ini");
             f.SetOBMMVersion(1, 1, 12);
             f.SetOutputDirectory(@"M:\Projects\omod\testDLL\out");
-            f.SetPluginsListPath("");
+            f.SetPluginsListPath(@"C:\Users\Florian\AppData\Local\Oblivion\Plugins.txt");
             f.SetTempDirectory(@"M:\Projects\omod\testDLL\temp");
             f.SetDLLPath(@"M:\Projects\OMOD-Extractor\OMOD-Framework\bin\Release\erri120.OMODFramework.dll");
-            OMOD omod = new OMOD(@"M:\Projects\omod\testDLL\DarkUId DarN 16.omod", f);
+            OMOD omod = new OMOD(@"M:\Projects\omod\testDLL\Oblivion XP v415 - OMOD-15619.omod", f);
             Console.WriteLine(omod.ExtractDataFiles());
             Console.WriteLine(omod.ExtractPlugins());
             ScriptRunner sr = new ScriptRunner(omod);
-            sr.ExecuteScript(null, null, null, null, null, null, null, null, null, null, null);
+            string[] activeESP = new string[1]{ "oblivion.esm" };
+            sr.ExecuteScript(
+                (s1)=> { Console.WriteLine(s1); }, //warn
+                (s1,s2)=> { return 1; }, //dialogYesNo
+                (s1) => { return true; }, //existsFile
+                null, // getFileVersion
+                (s1,s2,b1,s3,s4)=> { return new int[1] { 1 }; }, //dialogSelect
+                null, //message
+                null, //displayImage
+                null, //displayText
+                (s1,s2)=> { return "Hi"; }, //inputString
+                ()=> { return activeESP; }, //getActiveESPNames
+                (s1) => { return Path.Combine(data,s1); } //getFileFromPath
+                );
             Console.WriteLine("hi");
         }
     }
