f5ebde564db0611d68ec5f58cbb2d776b6f1c6f6
erri120 <erri120@protonmail.com>
2019-09-30 15:09:31 +0200

    Implemented all Read functions

---
+++ OMOD-Framework/Classes/OblivionINI.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OMODFramework
+{
+    internal static class OblivionINI
+    {
+        private static readonly string ini = Framework.OblivionINIPath;
+
+        internal static string GetINIValue(string section, string name)
+        {
+            string[] ss = GetINISection(section);
+            if (ss != null)
+            {
+                name = name.ToLower();
+                foreach (string s in ss)
+                {
+                    if (s.Trim().ToLower().StartsWith(name + "="))
+                    {
+                        string res = s.Substring(s.IndexOf('=') + 1).Trim();
+                        int i = res.IndexOf(';');
+                        if (i != -1) res = res.Substring(0, i - 1);
+                        return res;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static string[] GetINISection(string section)
+        {
+            List<string> contents = new List<string>();
+            bool InSection = false;
+            section = section.ToLower();
+            StreamReader sr = new StreamReader(File.OpenRead(ini), System.Text.Encoding.Default);
+            try
+            {
+                while (sr.Peek() != -1)
+                {
+                    string s = sr.ReadLine();
+                    if (InSection)
+                    {
+                        if (s.Trim().StartsWith("[") && s.Trim().EndsWith("]")) break;
+                        contents.Add(s);
+                    }
+                    else
+                    {
+                        if (s.Trim().ToLower() == section) InSection = true;
+                    }
+                }
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+            if (!InSection) return null;
+            return contents.ToArray();
+        }
+
+        private static void CreateINITweak(string section, string name, string value)
+        {
+            string sectionName = section.Replace("[", "").Replace("]", "");
+            List<string> contents = new List<string>();
+            string tweakPath = Path.Combine(Framework.OutputDir, sectionName + "_tweak.ini");
+            if (File.Exists(tweakPath))
+            {
+                using (StreamReader sr = new StreamReader(tweakPath, System.Text.Encoding.Default))
+                {
+                    while (sr.Peek() != -1)
+                    {
+                        string s = sr.ReadLine();
+                        contents.Add(s);
+                    }
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(File.Create(tweakPath), System.Text.Encoding.Default))
+            {
+                if (!contents.Contains(section)) contents.Add(section);
+                contents.Add(name + "=" + value);
+                foreach (string s in contents)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+        }
+    }
+}
--- OMOD-Framework/Scripting/obmmScriptHandler.cs
+++ OMOD-Framework/Scripting/obmmScriptHandler.cs
@@ -2039,6 +2039,36 @@ namespace OMODFramework.Scripting
             else variables[line[1]] = result;
         }
 
+        private static void FunctionReadINI(string[] line)
+        {
+            if (line.Length < 4)
+            {
+                Warn("Missing arguments to function ReadINI");
+                return;
+            }
+            if (line.Length > 4) Warn("Unexpected extra arguments to function ReadINI");
+            try
+            {
+                variables[line[1]] = OblivionINI.GetINIValue(line[2], line[3]);
+            }
+            catch (Exception e) { variables[line[1]] = e.Message; }
+        }
+
+        private static void FunctionReadRenderer(string[] line)
+        {
+            if (line.Length < 3)
+            {
+                Warn("Missing arguments to function 'ReadRendererInfo'");
+                return;
+            }
+            if (line.Length > 3) Warn("Unexpected extra arguments to function 'ReadRendererInfo'");
+            try
+            {
+                variables[line[1]] = "";
+            }
+            catch (Exception e) { variables[line[1]] = e.Message; }
+        }
+
         private static int iSet(List<string> func)
         {
             if (func.Count == 0) throw new Exception("Empty iSet");
